b2cc0a129331824b82b3f12fbf2e87c977876bc5
doyelghoshhajra <doyelghoshhajra@gmail.com>
2023-08-06 16:44:13 -0700

    dense_rank exercise added

--- employeeSqlPractice.sql
+++ employeeSqlPractice.sql
@@ -67,6 +67,37 @@ SELECT max(datediff(CURDATE(), employees.hire_date)) AS max_exp, employees.first
      where employees.department_id = departments.department_id
      GROUP BY departments.department_name;
 
+#8 Find third highest experienced employee per department
+WITH RankedEmployees AS (
+    SELECT e.employee_id, e.first_name, e.last_name, e.hire_date, d.department_name,
+	DENSE_RANK() OVER (PARTITION BY d.department_name ORDER BY e.hire_date) AS experience_dense_rank
+    FROM interview.employees e
+    JOIN interview.departments d
+    ON e.department_id = d.department_id
+)
+SELECT re.employee_id, re.first_name, re.last_name, re.hire_date, re.department_name
+FROM RankedEmployees re
+WHERE re.experience_dense_rank = 3;
+
+-- In this query:
+-- The RankedEmployees common table expression (CTE) is created by joining the employees and departments tables using the department_id field.
+-- This allows you to include the department_name in the result set.
+-- The DENSE_RANK() window function is used within the CTE
+-- 						to assign a dense rank to each employee within their respective department, ordered by their hire date.
+-- The main query selects the employee details from the RankedEmployees CTE where the experience_dense_rank is equal to 3,
+-- 						which corresponds to the third highest experienced employee in each department.
+-- This query provides you with the third highest experienced employee per department_name
+-- 						using the DENSE_RANK() function, a CTE, and appropriate joins between the two tables.
+-------------------------------------------------------
+
+#9 Select the job with maximum salary
+select employees.salary, departments.department_name
+from interview.employees, interview.departments
+where employees.department_id = departments.department_id
+order by employees.salary desc limit 1;
+
+
+
 
 
 
